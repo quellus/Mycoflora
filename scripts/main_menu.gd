f37fe794ceabcf287090c0425791d0316efd6e45
quellus <quellus7@gmail.com>
2024-08-09 17:55:52 -0600

    Add version number and title to main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,4 +1,8 @@
 extends Control
 
+func _ready() -> void:
+	$Label.text = "Version " + ProjectSettings.get_setting("application/config/version")
+
+
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
