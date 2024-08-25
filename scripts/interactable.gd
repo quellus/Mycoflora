a0411efc06d64e2547e011860f893b9e62400001
quellus <quellus7@gmail.com>
2024-08-25 09:52:09 -0600

    Added flowers

---
+++ scripts/interactable.gd
@@ -0,0 +1,18 @@
+class_name Interactable extends Area2D
+
+const TOTAL_FLOWERS: int = 15
+const FLOWER_COLUMNS: int = 3
+@onready var flower_sprite: Sprite2D = $Sprite2D
+
+func _ready():
+	var column = 0
+	var flower_index = randi_range(0, TOTAL_FLOWERS -1)
+	print(flower_index)
+	if flower_index > 5:
+		flower_index -= 5
+		column += 1
+	if flower_index > 5:
+		flower_index -= 5
+		column += 1
+	flower_sprite.texture.region.position.x = flower_index * 16
+	flower_sprite.texture.region.position.y = column * 16
