f86c19c5d24971dedc306e32bbd07563fc0f3017
quellus <quellus7@gmail.com>
2024-09-13 18:46:08 -0600

    Now storing data in an autoload and dumping it to a file

--- scripts/save_load.gd
+++ scripts/save_load.gd
@@ -1,11 +1,16 @@
 extends Node
 
-var data = {}
+var data = {
+	"players": {
+		"player1": {
+			"learned_magic": false,
+			"has_weapon": false,
+			"flowers": 10
+		}
+	}
+}
 
 func save_game():
-	var save_nodes = get_tree().get_nodes_in_group("save")
-	for save_node in save_nodes.save_data:
-		data.merge(save_node.save_data)
 	var json_data = JSON.stringify(data)
-	var file = FileAccess.open("user://save_game.dat", FileAccess.WRITE)
+	var file = FileAccess.open("user://save_game.json", FileAccess.WRITE)
 	file.store_string(json_data)
