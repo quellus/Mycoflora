5663efe419a6a2375f2d6081570dc1e629098da7
quellus <quellus7@gmail.com>
2024-10-06 15:44:50 -0600

    Enemies continue to attack while you're in their radius

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -4,6 +4,7 @@ class_name Enemy extends CharacterBody2D
 
 var health = 10
 var target: Player = null
+var is_in_range: bool = false
 @onready var sprite = $AnimatedSprite2D
 @export var speed_mod := 1
 const SPEED = 70
@@ -15,7 +16,7 @@ const SPEED = 70
 func _physics_process(_delta):
 	if target != null:
 		var next_pos: Vector2 = nav_agent.get_next_path_position()
-		if (target.global_position.distance_to(nav_agent.target_position) >= 50 or nav_agent.is_navigation_finished()):
+		if (target.global_position.distance_to(nav_agent.target_position) >= 30 or nav_agent.is_navigation_finished()):
 			nav_agent.target_position = target.global_position
 			next_pos = nav_agent.get_next_path_position()
 		velocity = global_position.direction_to(next_pos) * SPEED * speed_mod
@@ -51,10 +52,24 @@ func _on_hurt_detector_area_entered(area):
 
 func _on_attack_radius_body_entered(body: Node2D) -> void:
 	if body is Player and health > 0:
+		is_in_range = true
 		target = body
 		state_machine.travel("attack")
+		$Timer.start(5)
+
+
+func _on_attack_radius_body_exited(body: Node2D) -> void:
+	if body is Player:
+		is_in_range = false
 
 
 func _on_hurt_box_body_entered(body: Node2D) -> void:
 	if body == target and animplayer.current_animation == "attack":
 			state_machine.travel("default")
+
+
+func _on_attack_timer_timeout() -> void:
+	if target and is_in_range:
+		state_machine.travel("attack")
+	else:
+		$Timer.stop()
