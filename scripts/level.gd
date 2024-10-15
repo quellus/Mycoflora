8caebc2988ca9fb6b82a9517a5bac87d48c85c94
quellus <quellus7@gmail.com>
2024-10-15 16:27:52 -0600

    Fixed nav crashing in castle

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,7 +1,9 @@
 class_name Level extends Node2D
 
 @export var spawn_points: Array[Node2D]
+@export var nav_region: NavigationRegion2D
 var spawn := Vector2(0, 0)
 
 func _ready():
-	$NavigationRegion2D.bake_navigation_polygon()
+	if nav_region:
+		nav_region.bake_navigation_polygon()
