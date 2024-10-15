007abd2bb78fc28f13c62cfbc1586ffdae90dcdc
quellus <quellus7@gmail.com>
2024-10-15 16:29:29 -0600

    Update princess dialogue and add dialog sounds

--- scripts/dialog_manager.gd
+++ scripts/dialog_manager.gd
@@ -3,7 +3,14 @@ class_name DialogueManager extends Control
 @export var dialogue_trees: Dictionary = {}
 
 @onready var dialogue_box: DialogueBox = %DialogueBox
+@onready var audio_player: AudioStreamPlayer = %DialogueAudioPlayer
 
+var current_dialogue: String = ""
+var silent_characters: String = ",.?/ -_!@#$%^&*()~`"
+
+func _ready() -> void:
+	dialogue_box.custom_effects[0].char_displayed.connect(_on_char_displayed)
+	dialogue_box.dialogue_processed.connect(_on_dialogue_processed)
 
 func start(char_name: String):
 	if char_name in dialogue_trees:
@@ -15,6 +22,15 @@ func start(char_name: String):
 		printerr("Dialogue requested but ", char_name, " not found in dialogue trees")
 
 func _update_dialog_variables(data: DialogueData):
-	print("updating variables")
 	if "KILLED_BOSS" in data.variables:
 		data.variables["KILLED_BOSS"]["value"] = SaveLoad.data["players"]["player1"]["killed_boss"]
+
+func _on_char_displayed(char_index: int):
+	if char_index % 2 == 0:
+		if !current_dialogue[char_index] in silent_characters:
+			audio_player.pitch_scale = 2 + randf_range(-0.1, 0.1)
+			print(current_dialogue[char_index])
+			audio_player.play()
+
+func _on_dialogue_processed(_speaker : Variant, dialogue : String, _options : Array[String]):
+	current_dialogue = dialogue
