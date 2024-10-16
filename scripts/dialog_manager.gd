2775d1b6f9980e1b297fab598da91d8ed3852d87
quellus <quellus7@gmail.com>
2024-10-15 18:11:04 -0600

    Added styling and font to dialogue box

--- scripts/dialog_manager.gd
+++ scripts/dialog_manager.gd
@@ -3,12 +3,15 @@ class_name DialogueManager extends Control
 @export var dialogue_trees: Dictionary = {}
 
 @onready var dialogue_box: DialogueBox = %DialogueBox
+@onready var nine_patch: NinePatchRect = %NinePatchRect
 @onready var audio_player: AudioStreamPlayer = %DialogueAudioPlayer
 
 var current_dialogue: String = ""
 var silent_characters: String = ",.?/ -_!@#$%^&*()~`"
 
 func _ready() -> void:
+	dialogue_box.visible = false
+	nine_patch.visible = false
 	dialogue_box.custom_effects[0].char_displayed.connect(_on_char_displayed)
 	dialogue_box.dialogue_processed.connect(_on_dialogue_processed)
 
@@ -34,3 +37,11 @@ func _on_char_displayed(char_index: int):
 
 func _on_dialogue_processed(_speaker : Variant, dialogue : String, _options : Array[String]):
 	current_dialogue = dialogue
+
+
+func _on_dialogue_box_dialogue_started(_id: String) -> void:
+	nine_patch.visible = true
+
+
+func _on_dialogue_box_dialogue_ended() -> void:
+	nine_patch.visible = false
