68810fe7aa56cf6c3aa0a609d0bc4039ff43bbfc
quellus <quellus7@gmail.com>
2024-10-29 19:45:31 -0600

    Changed the heart indicators to animated sprites

--- health_bar.gd
+++ health_bar.gd
@@ -1,54 +1,38 @@
 class_name HealthBar extends Control
 
-const HEART_TEXTURE = preload("res://assets/Heart.png")
-const HEART_EMPTY_TEXTURE = preload("res://assets/HeartEmpty.png")
-
-var hearts: Array[TextureRect] = []
+var hearts: Array[AnimatedSprite2D] = []
 
 var value: int = 10:
 	set(val):
 		if value != val:
 			value = val
 			update_visual()
-		
+
+
 var max_value: int = 10:
 	set(val):
 		if max_value != val:
 			max_value = val
 			update_visual()
 
+
 func _ready() -> void:
-	initialize_hearts()
+	for heart in get_children():
+		if heart is AnimatedSprite2D:
+			hearts.append(heart)
+	update_visual()
 
 
 func update_visual():
-	if hearts.size() > max_value:
-		initialize_hearts()
-		return
-	elif hearts.size() < max_value:
-		for i in range(max_value - hearts.size()):
-			create_heart(i - 1 + max_value)
 	for i in range(hearts.size()):
-		if i >= value:
-			hearts[i].texture = HEART_EMPTY_TEXTURE
+		var heart: AnimatedSprite2D = hearts[i]
+		if i >= max_value:
+			heart.visible = false
+		elif i >= value:
+			heart.visible = true
+			if heart.animation != "empty":
+				heart.play("empty")
 		else:
-			hearts[i].texture = HEART_TEXTURE
-
-
-func initialize_hearts():
-	for child in $HBoxContainer.get_children():
-		child.queue_free()
-	for i in range(max_value):
-		create_heart(i)
-
-
-func create_heart(index: int):
-	var texture_rect = TextureRect.new()
-	texture_rect.stretch_mode = TextureRect.STRETCH_KEEP
-	
-	if index >= value:
-		texture_rect.texture = HEART_EMPTY_TEXTURE
-	else:
-		texture_rect.texture = HEART_TEXTURE
-	hearts.append(texture_rect)
-	$HBoxContainer.add_child(texture_rect)
+			heart.visible = true
+			if heart.animation != "full":
+				heart.play("full")
